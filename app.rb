9e2a74cae107884d9abdbfeb7fb821a4ca2d09b3
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-19 20:47:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,9 +12,13 @@ module FormsLab
     end
 
     post '/pirates' do
-    @pirate = Pirate.new(params[:pirate])
-    params[:pirate][:ships].each do |details|
-      Ship.new(details)
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      erb :"/pirates/show"
     end
 
   end
