2c62fed8d1e4b0e9a8b267714285919ec6d74d01
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-19 21:00:36 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -6,7 +6,7 @@ class Ship
   def initialize(params)
     @name = params[:name]
     @type = params[:type]
-    @booty_attributes = params[:booty]
+    @booty = params[:booty]
 
     SHIPS << self
   end
